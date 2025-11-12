54ea9f6bb6fcb7668c2d7f61b2b479b6f61db4e4
Chaitrali Bhosale <chaitralibhosale14@gmail.com>
2025-11-12 22:38:01 +0530

    Commited

--- src/main/java/com/Baseclass/Library.java
+++ src/main/java/com/Baseclass/Library.java
@@ -1,5 +1,48 @@
 package com.Baseclass;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.*;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Library {
+//launching browers etc
+	public static Properties prop;
+	public static WebDriver driver;
+	public void launchapp() throws IOException{
+		FileInputStream input =new FileInputStream("/Flipkart_Automation_Test/src/test/resources/Properties/config.properties");
+		prop=new Properties();
+		
+		prop.load(input);
+		try {
+			if (prop.getProperty("browser").equalsIgnoreCase("chrome")) {
+				WebDriverManager.chromedriver().setup();
+				driver=new ChromeDriver();
+				
+				
+			}
+			else if(prop.getProperty("browser").equalsIgnoreCase("firefox")) {
+				WebDriverManager.firefoxdriver().setup();
+			driver=new FirefoxDriver();
+		}
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(null);
+		driver.get(prop.getProperty("url"));
+		
+	}catch(Exception e) {
+		System.out.println("brower launching failed");
+		}
+}
+public void teardown()	{
+	driver.close();
+	
+}
 
 }
+
--- src/main/java/com/ResuableFunc/SeleniumReusables.java
+++ src/main/java/com/ResuableFunc/SeleniumReusables.java
@@ -1,5 +1,64 @@
 package com.ResuableFunc;
 
-public class SeleniumReusables {
+import java.io.File;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import com.Baseclass.Library;
+
+public class SeleniumReusables extends Library {
+	
+	 public SeleniumReusables(WebDriver driver) {
+		// TODO Auto-generated constructor stub
+		 this.driver=driver;
+		 
+		 
+	} 
+	 
+	 //enter the text 
+	public void Entervalue(WebElement element, String Text) {
+		try {
+			element.sendKeys(Text);
+			
+		}
+		catch (Exception e){
+			System.out.println("no such element found");
+		}
+	}
+
+	//clicks the element on webpage
+	public void click(WebElement element) 
+	{
+		try {
+			
+			element.click();
+		}catch(Exception e)
+		{
+			System.out.println("no such element");
+		}
+		}
+	//gets the title of page
+	public void gettitle() {
+		try {
+			System.out.println(driver.getTitle());
+		}catch(Exception e)
+		{
+			System.out.println("could not get title");
+		}
+	}
+	//takes the screenshot of the testcase and stores in destined path
+	public void screenshot(String path) {
+		TakesScreenshot TS=(TakesScreenshot)driver;
+		File source=TS.getScreenshotAs(OutputType.FILE);
+		try {
+			FileUtils.copyFile(source, new File(path));
+		}
+		catch(Exception e){
+			System.out.println("Screenshot not found");
+		}
+	}
 }
